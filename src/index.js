113b79863cae5995585f261d51111522ea790892
Sunya Dickman <sunya.dickman@aerian.com>
2019-12-02 11:11:18 +0000

    Added non-colo test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 const sharp = require("sharp");
 const path = require("path");
 
+console.log(
+  "1. Most simple test, input and output are in the same location as the script - ",
+  path.resolve(__dirname, "input.jpg")
+);
 sharp(path.resolve(__dirname, "input.jpg"))
   .resize(300, 200)
-  .toFile(path.resolve(__dirname, "output.jpg"), function(err) {
-    console.error(err);
+  .toFile(path.resolve(__dirname, "output.jpg"), function(err, info) {
+    if (err) {
+      console.error(err);
+    }
+    console.log("1. simple test complete: ", info);
+  });
+
+console.log(
+  "2. Input and output are not colocated with script - ",
+  path.resolve(__dirname, "../images/input.jpg")
+);
+sharp(path.resolve(__dirname, "../images/input.jpg"))
+  .resize(300, 200)
+  .toFile(path.resolve(__dirname, "../images/output-not-colo.jpg"), function(
+    err,
+    info
+  ) {
+    if (err) {
+      console.error(err);
+    }
+    console.log("2. not colo complete. ", info);
   });
